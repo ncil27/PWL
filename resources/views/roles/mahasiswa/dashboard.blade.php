f7c2cedfcb35b8602c5f68e68502241ab27015f8
lauraa15 <laurapuspaa15@gmail.com>
2025-03-25 13:04:19 +0700

    tambahin card ke mahasiswa
    card blm diapa apain, baru nambahin componennya aja

--- resources/views/roles/mahasiswa/dashboard.blade.php
+++ resources/views/roles/mahasiswa/dashboard.blade.php
@@ -7,4 +7,24 @@
 <h3>Halo Mahasiswa!!!</h3>
 <p>Ini adalah halaman dashboard untuk kamu hey mahasiswa.</p>
 
+<div class="col-md-6 col-sm-6">
+    <div class="card">
+        <div class="card-content">
+            <div class="card-body">
+                <h4 class="card-title">Top Image Cap</h4>
+                <p class="card-text">
+                    Jelly-o sesame snaps cheesecake topping. Cupcake fruitcake macaroon donut
+                    pastry gummies tiramisu chocolate bar muffin. Dessert bonbon caramels brownie chocolate
+                    bar
+                    chocolate tart dragée.
+                </p>
+                <p class="card-text">
+                    Cupcake fruitcake macaroon donut pastry gummies tiramisu chocolate bar muffin.
+                </p>
+                <button class="btn btn-primary block">Update now</button>
+            </div>
+        </div>
+    </div>
+</div>
+
 @endsection
