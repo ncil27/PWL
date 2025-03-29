63dd36f73dfc438f2c4e16e79c81ca6389c40d8e
ncil27 <2372060@maranatha.ac.id>
2025-03-29 09:32:48 +0700

    lupa update apa aja tp gw mo masuk ke branch lain

--- resources/views/coba-template/mahasiswa.blade.php
+++ resources/views/coba-template/mahasiswa.blade.php
@@ -18,6 +18,8 @@
     <link rel="shortcut icon" href="{{asset('templates/assets/images/favicon.svg')}}" type="image/x-icon">
 </head>
 
+
+
 <body>
     <div id="app">
         <div id="sidebar" class="active">
@@ -36,28 +38,28 @@
                     <ul class="menu">
                         <li class="sidebar-title">Menu</li>
 
-                        <li class="sidebar-item active ">
+                        <li class="sidebar-item {{ Request::is('dashboard')?'active' : '' }}  ">
                             <a href="/dashboard" class='sidebar-link'>
                                 <i class="bi bi-grid-fill"></i>
                                 <span>Dashboard</span>
                             </a>
                         </li>
 
-                        <li class="sidebar-item  ">
+                        <li class="sidebar-item {{ Request::is('pengantar-tugas')?'active':'' }}">
                             <a href="/pengantar-tugas" class='sidebar-link'>
                                 <i class="bi bi-file-earmark-medical-fill"></i>
                                 <span>Surat Pengantar Tugas</span>
                             </a>
                         </li>
 
-                        <li class="sidebar-item  ">
+                        <li class="sidebar-item {{ Request::is('keterangan-ma')?'active':'' }} ">
                             <a href="" class='sidebar-link'>
                                 <i class="bi bi-file-earmark-medical-fill"></i>
                                 <span>Surat Keterangan Mahasiswa Aktif</span>
                             </a>
                         </li>
 
-                        <li class="sidebar-item  ">
+                        <li class="sidebar-item  {{ Request::is('keterangan-lulus')?'active':'' }}">
                             <a href="" class='sidebar-link'>
                                 <i class="bi bi-file-earmark-medical-fill"></i>
                                 <span>Surat Keterangan Lulus</span>
